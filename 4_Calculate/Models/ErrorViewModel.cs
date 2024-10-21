5a74ec79b2c9c9d6daff3bff9c3f09737b7849e3
Vadenka <125719521+Vadenka@users.noreply.github.com>
2024-10-21 13:07:52 +0500

    Delete 4_Calculate directory

--- 4_Calculate/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _4_Calculate.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
