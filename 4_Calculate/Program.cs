78c8a593880fc066a254c768a3e49bfae753d008
Vadenka <berlya.vlad@mail.ru>
2024-12-20 16:03:53 +0500

    Start of work "Prometeus и Grafana"

--- 4_Calculate/Program.cs
+++ 4_Calculate/Program.cs
@@ -1,7 +1,49 @@
+using OpenTelemetry.Metrics;
+using static System.Net.WebRequestMethods;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(meterProviderBuilder =>
+    {
+        meterProviderBuilder.AddPrometheusExporter();
+
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+            "Microsoft.AspNetCore.Server.Kestrel");
+
+        // Status code 
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
+
+
+        meterProviderBuilder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries =
+                [
+                    0,
+                    0.005,
+                    0.01,
+                    0.025,
+                    0.05,
+                    0.075,
+                    0.1,
+                    0.25,
+                    0.5,
+                    0.75,
+                    1,
+                    2.5,
+                    5,
+                    7.5,
+                    10
+                ]
+            });
+    });
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
@@ -15,6 +57,8 @@ if (!app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.MapPrometheusScrapingEndpoint();
 
 app.UseRouting();
 
--- 4_Calculate/obj/Debug/net6.0/4_Calculate.AssemblyInfo.cs
+++ 4_Calculate/obj/Debug/net6.0/4_Calculate.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4_Calculate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea32577ec2293e56e1108dfba3e55071580633b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("4_Calculate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4_Calculate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
